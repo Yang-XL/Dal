8a32ba3791863ab08a8a45d7f5079fd82e996f3e
YXL <499844677@qq.com>
2022-06-30 14:24:54 +0800

    T4

--- tests/mock/Mock.Entitys/DapperTest/UserDbEntity.cs
+++ tests/mock/Mock.Entitys/DapperTest/UserDbEntity.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Yxl.Dal.Aggregate;
+﻿using Yxl.Dal.Aggregate;
 using Yxl.Dal.Attributes;
 
 namespace Mock.Entitys.DapperTest
 {
+    
+    /// <summary>
+    /// 
+    /// </summary>
     [DB("dapper_test")]
     public class UserDbEntity : IEntity
     {
